f720518cb92047e9ff8c40c8351e1e5c467575b1
droidfivex <droidfivex@gmail.com>
2016-10-17 19:56:07 +0000

    inherit full_kltekdi.mk

--- aicp.mk
+++ aicp.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, device/samsung/kltekdi/full_kltekdi.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
