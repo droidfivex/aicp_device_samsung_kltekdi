ecbc3b0360bb02d2ca70940cde9067d2bec9472d
droidfivex <droidfivex@gmail.com>
2016-10-18 08:12:44 +0000

    awesome product configuration

--- aicp.mk
+++ aicp.mk
@@ -9,6 +9,13 @@ $(call inherit-product, vendor/aicp/configs/common.mk)
 # Inherit telephony stuff.
 $(call inherit-product, vendor/aicp/configs/telephony.mk)
 
+# Setup device specific product configuration
+PRODUCT_NAME := aicp_kltekdi
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := kltekdi
+PRODUCT_MODEL := SCL23
+PRODUCT_MANUFACTURER := samsung
+
 # AICP Device Maintainer
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	DEVICE_MAINTAINERS="5x (droidfivex)"
@@ -17,6 +24,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
 -include vendor/aicp/configs/bootanimation.mk
-
-PRODUCT_DEVICE := kltekdi
-PRODUCT_NAME := aicp_kltekdi
