e0712d3d63be5720256655af1fcb4d330cdc65ab
droidfivex <droidfivex@gmail.com>
2016-12-11 23:23:17 +0900

    kltekdi: hello AICP!
    Change-Id: Iab573981b730cc31af06aa93a59a51f858b3536f
    Signed-off-by: droidfivex <droidfivex@gmail.com>

--- cm.mk
+++
@@ -1,7 +0,0 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, device/samsung/kltekdi/full_kltekdi.mk)
-
-PRODUCT_DEVICE := kltekdi
-PRODUCT_NAME := cm_kltekdi
